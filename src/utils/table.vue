ecff99b493ee18a96fd9d59186164fac680de4a7
hanxue <hanxue@cmhi.chinamobile.com>
2023-07-14 14:21:28 +0800

    Opt eslint修改

---
+++ src/utils/table.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-pagination
+      v-model:current-page=props.pageNum
+      v-model:page-size=props.pageSize
+      :page-sizes="[10, 20, 30, 40]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total=props.total
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </template>
+
+  <script lang="ts" setup>
+  import {ref} from 'vue'
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`)
+  emits('refresh')
+}
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`)
+  emits('refresh')
+}
+
+const emits = defineEmits(['refresh'])
+const props = defineProps({
+  pageNum: {
+    type: Number,
+    default: 1,
+  },
+  pageSize: {
+    type: Number,
+    default: 10,
+  },
+  total: {
+    type: Number,
+    default: 0,
+  },
+})
+
+</script> 
